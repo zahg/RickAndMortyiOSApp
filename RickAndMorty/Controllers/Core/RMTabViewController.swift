f14a4e7b8ce17913890566579151008fb5ed810b
Parmjit Bal <parmjit@backbase.com>
2023-03-17 14:16:24 +0000

    Create Rick and Morty Project

--- RickAndMorty/Controllers/Core/RMTabViewController.swift
+++ RickAndMorty/Controllers/Core/RMTabViewController.swift
@@ -11,7 +11,8 @@ final class RMTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+ //       view.backgroundColor = .green
+        setUpTabs()
     }
 
     private func setUpTabs() {
@@ -35,13 +36,11 @@ final class RMTabBarController: UITabBarController {
         nav1.tabBarItem = UITabBarItem(title: "Locations", image: UIImage(systemName: "globe"), tag: 2)
         nav1.tabBarItem = UITabBarItem(title: "Episodes", image: UIImage(systemName: "tv"), tag: 3)
         nav1.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gear"), tag: 4)
-
         
         for nav in [nav1, nav2, nav3, nav4] {
             nav.navigationBar.prefersLargeTitles = true
         }
 
-        
         setViewControllers(
             [nav1, nav2, nav3, nav4],
             animated: true
